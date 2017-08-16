78cee8f286035f9a1a64d84446df706096ac4a83
GaetanoP17 <g.prisco21@studenti.unisa.it>
2017-08-16 19:13:47 +0200

    Initial commit

---
+++ www/js/routes.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Gaetano on 15/08/2017.
+ */
+
+angular.module('starter.routes', [])
+
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
+        }
+      })
+
+      .state('app.browse', {
+        url: '/browse',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/map.html',
+            controller: 'MapCtrl'
+          }
+        }
+      })
+      .state('app.playlists', {
+        url: '/playlists',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlists.html',
+            controller: 'PlaylistsCtrl'
+          }
+        }
+      })
+
+      .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/playlists');
+  });
---
+++ www/js/services.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Gaetano on 16/08/2017.
+ */
+
+angular.module('starter.services', [])
+
+
+
